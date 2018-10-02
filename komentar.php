71c7d1776045dfc969c9f9ee32ec6cc54e95b195
Dian Widya Putri <43510248+dianwp@users.noreply.github.com>
2018-10-02 16:32:22 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<center><h2>Komentar</h2><br>
+	<form method="POST">
+		<table>
+			<tr><input type="text" name="komentar" placeholder="komentar" required></tr><br><br>
+			<tr><input type="submit" name="submit" value="hitung kata"></tr><br><br>
+		</table>
+	</form>
+</body>
+</html>
+	</center>
+</form>
+
 <?php
+	if(isset($_POST['submit'])){
+		$komentar = $_POST['komentar'];
+		$hasil = str_word_count($komentar);
+			echo "Jumlah kata pada komentar  adalah : $hasil";
+	}
 
 ?>
